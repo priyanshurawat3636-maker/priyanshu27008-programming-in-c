96a8e940a1a9e6a6af1a62c28ff4054b53908d5c
priyanshurawat3636-maker <priyanshurawat3636@gmail.com>
2025-08-31 22:58:21 +0530

    Experiment 1.2.c

---
+++ Experiment 1.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+printf("\n--------------------------------------\n");
+int a;
+int b;
+int SUM;
+printf("Enter the value of number 1 ");
+scanf("%d",&a);
+printf("Enter the value of number 2 ");
+scanf("%d",&b);
+SUM=a+b;
+printf("The sum of %d and %d is: %d\n", a, b, SUM);
+return 0;
+}
+printf ("Name- PRIYANSHU\nSAP_ID-590028007\nCourse-BCA\nBatch-5");
