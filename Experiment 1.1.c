80d67a708dc7ff613a51879ddf44403a40088fda
priyanshurawat3636-maker <priyanshurawat3636@gmail.com>
2025-08-31 22:45:46 +0530

    Experiment 1.1.c

---
+++ Experiment 1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+printf("\n--------------------------------------\n");
+printf ("Name- PRIYANSHU\nSAP_ID-590028007\nCourse-BCA\nBatch-5");
+printf("SHARDA \n");
+printf("Street : NIMBUCHAUR\n");
+printf("City : KOTDWARA\n");
+printf("State : UTTARAKHAND\n");
+printf("Pincode : 246149\n");
+return 0;
+}
