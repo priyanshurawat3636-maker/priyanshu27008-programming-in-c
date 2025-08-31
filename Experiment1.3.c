3e0879562c1731ac70d61bb3184b3525c630bc6a
priyanshurawat3636-maker <priyanshurawat3636@gmail.com>
2025-08-31 23:00:30 +0530

    Experiment1.3.c

---
+++ Experiment1.3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+printf ("Name- PRIYANSHU\nSAP_ID-590028007\nCourse-BCA\nBatch-5");
+printf("\n--------------------------------------\n");
+int a;
+int b;
+int SUM;
+int SUB;
+int MUL;
+int DIV;
+printf("Enter the value of number 1 ");
+scanf("%d",&a);
+printf("Enter the value of number 2 ");
+scanf("%d",&b);
+SUM=a+b;
+printf("The sum of %d and %d is: %d\n", a, b, SUM);
+SUB=a-b;
+printf("The sub of %d and %d is: %d\n", a, b, SUB);
+MUL=a*b;
+printf("The mul of %d and %d is: %d\n", a, b, MUL);
+DIV=a/b;
+printf("The div of %d and %d is: %d\n", a, b, DIV);
+return 0;
+}
